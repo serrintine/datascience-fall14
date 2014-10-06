7a148894f0e484ca15b3afbd48364e4c73633e2c
serrintine <serrintine@gmail.com>
2014-10-05 21:54:01 -0400

    finised

--- lab2/count.py
+++ lab2/count.py
@@ -5,7 +5,7 @@ import pandas as pd
 reader = DataFileReader(open("countries.avro", "r"), DatumReader())
 
 countries = pd.DataFrame.from_records(reader)
-big_countries = countries[countries.population > 100000000]
+big_countries = countries[countries.population > 10000000]
 
 print(big_countries['name'].count())
 
