9e84a61d937effd8b63b149843e04e5a28bc1096
serrintine <serrintine@gmail.com>
2014-11-10 22:15:58 -0500

    .

--- lab6/part2/bigram_ranks.py
+++ lab6/part2/bigram_ranks.py
@@ -10,7 +10,7 @@ bigrams = text.flatMap(lambda x: [((x[i], x[i+1]), 1) for i in range(0, len(x)-1
 counts = bigrams.reduceByKey(lambda x, y: x + y)
 pairs = counts.flatMap(lambda x: [(x[0][0], (x[0], x[1])), (x[0][1], (x[0], x[1]))])
 ranks = pairs.combineByKey((lambda x: [x]), (lambda x, y: x + [y]), (lambda x, y: x + y))
-top = ranks.map(lambda x: (x[0], sorted(x[1], key=lambda x: x[1])[-5:][::-1]))
+top = ranks.map(lambda x: (x[0], sorted(x[1], key=lambda y: y[1], reverse=True)[:5]))
 
 with open('bigram_ranks.txt', 'w+') as f:
   sample = top.take(100)
